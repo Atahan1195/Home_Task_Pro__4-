21325b21b95dbab864ba297479724cf472af39e8
Atahan <atahan11951195@gmail.com>
2024-01-02 08:33:10 +0400

    Fix

--- main.py
+++ main.py
@@ -12,80 +12,40 @@
 # тип которой реализовать самостоятельно.
 
 
-class Product:
-    """
-    Product class
-    :param name: product name
-    :param price: product price
-    :param quantity: product quantity
-    """
-    def __init__(self, name, price, quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-
-        if self.price <= 0:
-            raise ValueError('Price must be greater than 0')
-
-        if self.quantity <= 0:
-            raise ValueError('Quantity must be greater than 0')
+class StudentsLimitError(Exception):
+    def __init__(self, max_students, message='Too many students'):
+        self.max_students = max_students
+        self.message = message
+        super().__init__()
 
     def __str__(self):
-        """
-        Returns product info
-        """
-        return f"{self.name}: {self.price * self.quantity}"
-
-    def apply_discount(self, discount_percentage):
-        """
-        Applies discount to the product
-        :param discount_percentage:
-        """
-        try:
-            if discount_percentage > 0 or discount_percentage <= 100:
-                discount_factor = 1 - discount_percentage / 100
-                self.price *= discount_factor
-        except ValueError as err:
-            print(err)
+        return f'Limit of students is {self.max_students}\n. {self.message}'
 
 
-class Order:
-    """
-    Order class
-    :param products: list of products
-    """
-    def __init__(self, products):
-        self.products = products
+class Student:
 
-    def get_total_sum(self):
-        """
-        Returns total sum of the order
-        """
-        self.total_sum = 0
-        for product in self.products:
-            self.total_sum += product.price * product.quantity
-        return self.total_sum
+    def __init__(self, name, date_of_birth, sex):
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.sex = sex
+
+    def __str__(self):
+        return f'{self.name}'
 
 
-try:
-    products = [Product('apple', 10, 2),
-                Product('banana', 20, 5),
-                Product('orange', 30, 4)]
+class Group:
 
-    order = Order(products)
+    def __init__(self, title, max_students=10):
+        self.title = title
+        self.max_students = max_students
+        self.__students = []
 
-    for product in products:
-        product.apply_discount(15)
+    def add_student(self, student: Student):
+        if not isinstance(student, Student):
+            raise TypeError('Not a Student')
+        if student in self.__students:
+            raise ValueError('Student already in group')
+        if len(self.__students) == self.max_students:
+            raise StudentsLimitError(self.max_students)
 
-    print("Products in the order:")
-    for product in products:
-        print(product)
-
-    print("\nTotal Order Cost (after discount):", order.get_total_sum())
-
-    invalid_discount_product = Product('invalid_discount_product', 50, 3)
-    invalid_discount_product.apply_discount(120)
-
-except ValueError as err:
-    print(err)
-
+        self.__students.append(student)
